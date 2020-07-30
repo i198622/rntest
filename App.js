065b693e774f2c0898e07cfb3d12fe5ecc57e7c0
Игорь <applecake@inbox.ru>
2020-07-30 11:56:56 +0300

    improve prettier config

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 function HomeScreen() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={styles.container}>
       <Text>Home Screen</Text>
     </View>
   );
@@ -22,5 +22,13 @@ function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
